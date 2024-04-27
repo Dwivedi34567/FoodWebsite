f7ff3c0c45921c67a065b0ccf955a234e28a815b
Prakhar Dwivedi <prakhard@chromeinfotech.com>
2024-04-27 14:14:49 +0530

    UI Integration of Website Completed

---
+++ FoodOrderingWebsite/FoodOrderingWebsite/Controllers/BookTableController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FoodOrderingWebsite.Controllers
+{
+    public class BookTableController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
